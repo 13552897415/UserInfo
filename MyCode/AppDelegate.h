a4fd1dbe70615d57dd9b85172c03515bf576bcf7
13552897415 <1164247314@qq.com>
2015-10-20 10:21:40 +0800

    first commit

---
+++ MyCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCode
+//
+//  Created by 唐僧 on 15-10-20.
+//  Copyright (c) 2015年 唐僧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCode
+//
+//  Created by 唐僧 on 15-10-20.
+//  Copyright (c) 2015年 唐僧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
